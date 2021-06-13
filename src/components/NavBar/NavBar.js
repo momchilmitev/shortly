6decc9c2adb1fb925ced5d83ea6fe11c90fed2b5
momchilmitev <momchilmitev87@gmail.com>
2021-06-13 12:04:50 +0200

    addind the stiles for the nav in mobile version

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,28 +1,41 @@
 import './NavBar.scss';
+import { useState } from 'react';
 
 import logo from '../../assets/images/logo.svg';
 import hamburger from '../../assets/icons/icon-hamburger.svg';
 
 const NavBar = () => {
+  const [toggleNavClass, setToggleNavClass] = useState(
+    'navigation__actions--close',
+  );
+
+  const toggleNav = () => {
+    toggleNavClass === 'navigation__actions--close'
+      ? setToggleNavClass('navigation__actions--open')
+      : setToggleNavClass('navigation__actions--close');
+  };
+
   return (
-    <nav>
+    <nav className="navigation">
       <img className="navigation__logo" src={logo} alt="logo" />
-      <section className="navigation__links">
-        <a className="navigation__link" href="#">
-          Features
-        </a>
-        <a className="navigation__link" href="#">
-          Pricing
-        </a>
-        <a className="navigation__link" href="#">
-          Resources
-        </a>
+      <section className={`navigation__actions ${toggleNavClass}`}>
+        <section className="navigation__links">
+          <a className="navigation__link" href="#">
+            Features
+          </a>
+          <a className="navigation__link" href="#">
+            Pricing
+          </a>
+          <a className="navigation__link" href="#">
+            Resources
+          </a>
+        </section>
+        <section className="navigation__buttons">
+          <button className="btn">Login</button>
+          <button className="btn btn--primary">Sign Up</button>
+        </section>
       </section>
-      <section className="navigation__buttons">
-        <button className="btn">Login</button>
-        <button className="btn--primary">Sign Up</button>
-      </section>
-      <img src={hamburger} alt="mobile navigation icon" />
+      <img onClick={toggleNav} src={hamburger} alt="mobile navigation icon" />
     </nav>
   );
 };
