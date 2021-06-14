305a74e36c430864df38468e051d27ca1755f72a
momchilmitev <momchilmitev87@gmail.com>
2021-06-14 19:23:12 +0200

    adding the statistics component with the basic markup

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,20 @@
+import './Statistics.scss';
+
+const Statistics = () => {
+  return (
+    <section className="statistics">
+      <div className="statistics__container">
+        <h3 className="statistics__title">Advanced Statistics</h3>
+        <p className="statistics__description">
+          Track how your links are performing across the web with our advanced
+          statistics dashboard.
+        </p>
+        <section className="statistics__cards">
+          <p className="line"></p>
+        </section>
+      </div>
+    </section>
+  );
+};
+
+export default Statistics;
