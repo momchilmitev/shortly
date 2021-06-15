78eea67b628c8ff562e7fdb3fbf0d5c5cac01492
momchilmitev <momchilmitev87@gmail.com>
2021-06-15 10:11:16 +0200

    adding the state data for the statistics and rendering it on the page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,14 @@ import './App.scss';
 
 import NavBar from '../NavBar/NavBar';
 import Hero from '../Hero/Hero';
+import Statistics from '../Statistics/Statistics';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Hero />
+      <Statistics />
     </div>
   );
 }
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,6 +1,31 @@
 import './Statistics.scss';
 import StatisticCard from '../StatisticCard/StatisticCard';
 
+import brandRecognitionIcon from '../../assets/icons/icon-brand-recognition.svg';
+import detailedRecordsIcon from '../../assets/icons/icon-detailed-records.svg';
+import fullyCustomizableIcon from '../../assets/icons/icon-fully-customizable.svg';
+
+const state = [
+  {
+    icon: brandRecognitionIcon,
+    title: 'Brand Recognition',
+    description:
+      'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.',
+  },
+  {
+    icon: detailedRecordsIcon,
+    title: 'Detailed Records',
+    description:
+      'Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.',
+  },
+  {
+    icon: fullyCustomizableIcon,
+    title: 'Fully Customizable',
+    description:
+      'Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.',
+  },
+];
+
 const Statistics = () => {
   return (
     <section className="statistics">
@@ -11,6 +36,13 @@ const Statistics = () => {
           statistics dashboard.
         </p>
         <section className="statistics__cards">
+          {state.map(({ icon, title, description }) => (
+            <StatisticCard
+              icon={icon}
+              title={title}
+              description={description}
+            />
+          ))}
           <p className="line"></p>
         </section>
       </div>
