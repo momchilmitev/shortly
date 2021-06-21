2507c83f4403851a9723e190db6b828b0fac667e
momchilmitev <momchilmitev87@gmail.com>
2021-06-21 20:37:42 +0200

    adding styles for the statistic card component in mobile version

--- src/components/StatisticCard/StatisticCard.js
+++ src/components/StatisticCard/StatisticCard.js
@@ -3,7 +3,9 @@ import './StatisticCard.scss';
 const StatisticCard = ({ icon, title, description }) => {
   return (
     <article className="card">
-      <img className="card__icon" src={icon} alt="card icon" />
+      <span className="card__icon">
+        <img src={icon} alt="card icon" />
+      </span>
       <h4 className="card__title">{title}</h4>
       <p className="card__description">{description}</p>
     </article>
