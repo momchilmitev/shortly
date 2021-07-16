45b9654b49aa50982c50a5cd0deebcddc0c98df2
momchilmitev <momchilmitev87@gmail.com>
2021-07-16 13:39:51 +0200

    adding the logic for fetching the short url

--- src/components/UrlInput/UrlInput.js
+++ src/components/UrlInput/UrlInput.js
@@ -1,16 +1,40 @@
 import './UrlInput.scss';
-import ShortenUrlList from "../ShortenUrlList/ShortenUrlList";
+import { useState } from 'react';
+import ShortenUrlList from '../ShortenUrlList/ShortenUrlList';
 
 const UrlInput = () => {
-    return (
-        <section className="shorten__container">
-            <form className="shorten__form">
-                <input className="shorten__input" type="text" placeholder="Shorten a link here..."/>
-                <button className="shorten__btn">Shorten It!</button>
-            </form>
-            <ShortenUrlList />
-        </section>
-    )
-}
+	const [url, setUrl] = useState('');
+	const [urls, setUrls] = useState([]);
+
+	const getShortUrl = (e) => {
+		e.preventDefault();
+		const apiUrl = `https://api.shrtco.de/v2/shorten?url=${url}`;
+		fetch(apiUrl)
+			.then((res) => res.json())
+			.then((data) => {
+				const readyUrl = {
+					shortUrl: data.result.full_short_link,
+					originalUrl: data.result.original_link,
+				};
+
+				setUrls([...urls, readyUrl]);
+			});
+	};
+
+	return (
+		<section className="shorten__container">
+			<form className="shorten__form" onSubmit={(e) => getShortUrl(e)}>
+				<input
+					className="shorten__input"
+					type="text"
+					placeholder="Shorten a link here..."
+					onChange={(e) => setUrl(e.target.value)}
+				/>
+				<button className="shorten__btn">Shorten It!</button>
+			</form>
+			<ShortenUrlList urls={urls} />
+		</section>
+	);
+};
 
 export default UrlInput;
