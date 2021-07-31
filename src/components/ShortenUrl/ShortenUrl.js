bd776ede7e23bd9c0c2881b38f8dd436a8807b42
momchilmitev <momchilmitev87@gmail.com>
2021-07-31 20:08:04 +0200

    adding the copy the short url into the clipboard functionality

--- src/components/ShortenUrl/ShortenUrl.js
+++ src/components/ShortenUrl/ShortenUrl.js
@@ -3,12 +3,20 @@ import { useState } from 'react';
 
 const ShortenUrl = ({ url }) => {
 	const [text, setText] = useState('Copy');
-	console.log(url);
+	const btnClass = text === 'Copy' ? 'url__btn' : 'url__btn url__btn--copied';
+
+	function copyShortUrlToClipboard() {
+		navigator.clipboard.writeText(url.shortUrl).then(() => setText('Copied!'));
+		setTimeout(() => setText('Copy'), 5000);
+	}
+
 	return (
 		<li className="url__container">
 			<p className="url__long">{url.shortUrl}</p>
 			<p className="url__short">{url.originalUrl}</p>
-			<button className="url__btn">{text}</button>
+			<button onClick={copyShortUrlToClipboard} className={btnClass}>
+				{text}
+			</button>
 		</li>
 	);
 };
